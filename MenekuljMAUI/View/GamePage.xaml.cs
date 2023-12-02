628ff4502dc1ae36f788e03b2e90c7f073e41a9a
zsonbi <zsomborder@gmail.com>
2023-12-02 20:31:36 +0100

    Fixed navigation problems

--- MenekuljMAUI/View/GamePage.xaml.cs
+++ MenekuljMAUI/View/GamePage.xaml.cs
@@ -6,11 +6,10 @@ public partial class GamePage : ContentPage
 	{
 
         InitializeComponent();
-        
     }
-
-    private void ContentPage_NavigatingFrom(object sender, NavigatingFromEventArgs e)
+    //Needed so the user can't outplay the pause system (I mean they would get in problem, so I'm a nice guy)
+    private void ContentPage_NavigatedFrom(object sender, NavigatedFromEventArgs e)
     {
-
+        ((Menekulj.ViewModel.ViewModel)BindingContext).PauseCommand.Execute(this);
     }
 }
